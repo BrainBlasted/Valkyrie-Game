a400f2ecf3544fdae25462805d8cb5feb6de2832
BrainBlasted <brainblastedmods@gmail.com>
2017-07-19 03:01:08 -0400

    Got a janky version of dashing working

--- Scenes/Characters/Angel/AngelPlaceholder.gd
+++ Scenes/Characters/Angel/AngelPlaceholder.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
 var speed = 70.0
-var velocity = Vector2()
+var velocityup = Vector2()
 var dashspeed = 70.0
+var velocity = Vector2()
 
 func _fixed_process(delta):
-	# move( Vector2(0,-1))
-	velocity.y -= delta * speed
-	var motion = delta * velocity
+	velocityup.y -= delta * speed
+	var motion = delta * velocityup
 	move(motion)
 	if Input.is_action_just_pressed("ui_down"):
 		_slow_y()
@@ -15,11 +15,13 @@ func _fixed_process(delta):
 		_speedup()
 	if Input.is_action_just_pressed("ui_right"):
 		velocity.x = dashspeed * 5.0
+		var motionx = velocity
+		move(motionx)
 	elif Input.is_action_just_pressed("ui_left"):
-		velocity.x =  -dashspeed * 5.0
-		if delta >= 1.0:
-			_stopx()
-			
+		velocity.x =  dashspeed * 5.0
+		var motionx = velocity
+		move(-motionx)
+
 func _slow_y():
 	# Slows the character
 	speed = speed * -1
