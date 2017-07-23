430d8bc9dce04490509f01753950da43f85b487a
BrainBlasted <brainblastedmods@gmail.com>
2017-07-23 10:47:46 -0400

    Added export template; Better horizontal movement

--- Scenes/Characters/Angel/AngelPlaceholder.gd
+++ Scenes/Characters/Angel/AngelPlaceholder.gd
@@ -2,33 +2,37 @@ extends KinematicBody2D
 
 var speed = 70.0
 var velocityup = Vector2()
-var dashspeed = 70.0
+var xspeed = 25.0
 var velocity = Vector2()
 
 func _fixed_process(delta):
-	velocityup.y -= delta * speed
+	velocityup.y -= delta * speed 
 	var motion = delta * velocityup
 	move(motion)
 	
-	velocity.x =  dashspeed * 5.0
+	velocity.x =  xspeed
 	var motionx = velocity
 
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed("ui_down") || Input.is_key_pressed(KEY_S): 
 		_slow_y()
-	elif Input.is_action_just_pressed("ui_up"):
+		# if !Input.is_key_pressed(KEY_S):
+		# 	speed = 70.0
+	elif Input.is_action_just_pressed("ui_up") || Input.is_key_pressed(KEY_W):
 		_speedup()
-	if Input.is_action_just_pressed("ui_right"):
+		# if !Input.is_key_pressed(KEY_W):
+		#	speed = 70.0
+	if Input.is_action_pressed("ui_right") || Input.is_key_pressed(KEY_D):
 		move(motionx)
-	elif Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") || Input.is_key_pressed(KEY_A):
 		move(-motionx)
 
 func _slow_y():
 	# Slows the character
-	speed = speed * -1
+	speed = speed - (speed * 0.75)
 
 func _speedup():
 	# Speeds up the character
-	speed = 300.0
+	speed = speed + (speed * 0.05)
 
 func _stopx():
 	velocity.x = 0
