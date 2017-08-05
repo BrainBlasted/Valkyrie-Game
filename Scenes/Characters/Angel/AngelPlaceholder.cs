e61988d2ae93d99fde088c9483f48df074277c29
BrainBlasted <brainblastedmods@gmail.com>
2017-08-05 00:41:35 -0400

    Changed AngelPlaceholder scen to use .cs

--- Scenes/Characters/Angel/AngelPlaceholder.cs
+++ Scenes/Characters/Angel/AngelPlaceholder.cs
@@ -3,10 +3,7 @@ using GodotEngine;
 
 public class AngelPlaceholder : KinematicBody2D
 {
-    // Member variables here, example:
-    // private int a = 2;
-    // private string b = "textvar";
-
+    // Motion variables
     float speed = 75.0f;
     float xspeed = 25.0f;
     Vector2 velocity;
@@ -15,22 +12,46 @@ public class AngelPlaceholder : KinematicBody2D
     {
         // Called every time the node is added to the scene.
         // Initialization here
+        set_fixed_process(true);
         
     }
 
+    void _fixed_process(float delta)
+    {
+        velocityup.y -= delta * speed;
+        Vector2 motion = delta * velocityup;
+        move(motion);
+
+        velocity.x = xspeed;
+        Vector2 motionx = velocity;
+
+        if (Input.is_action_pressed("ui_down") || Input.is_key_pressed(31))
+        {
+            SlowY();
+        } else if (Input.is_action_just_pressed("ui_up") || Input.is_key_pressed(17))
+        {
+            SpeedUp();
+        } else if (Input.is_action_pressed("ui_right") || Input.is_key_pressed(32))
+        {
+            move(motionx);
+        }else if (Input.is_action_pressed("ui_left") || Input.is_key_pressed(30))
+        {
+            move(-motionx);
+        }
+    }
     void SlowY()
     {
-
+        speed = speed - (speed * 0.75f);
     }
 
     void SpeedUp()
     {
-        
+        speed = speed + (speed * 0.05f);
     }
 
     void StopX()
     {
-
+        velocity.x = 0;
     }
 
     
