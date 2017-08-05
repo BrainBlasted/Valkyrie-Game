3ebee883c45d0b8ff8c104a4e43d78580fcff9fc
BrainBlasted <brainblastedmods@gmail.com>
2017-08-04 21:23:05 -0400

    Started C# file for AngelPlaceholder

---
+++ Scenes/Characters/Angel/AngelPlaceholder.cs
@@ -0,0 +1,37 @@
+using System;
+using GodotEngine;
+
+public class AngelPlaceholder : KinematicBody2D
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    float speed = 75.0f;
+    float xspeed = 25.0f;
+    Vector2 velocity;
+    Vector2 velocityup;
+    void _ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+
+    void SlowY()
+    {
+
+    }
+
+    void SpeedUp()
+    {
+        
+    }
+
+    void StopX()
+    {
+
+    }
+
+    
+}
